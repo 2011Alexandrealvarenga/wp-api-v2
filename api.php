5cbed8c8c899f4465d89bf66c7d5ac26b13f8046
2011 Alexandre <2011brunofernandes@gmail.com>
2022-12-27 12:04:28 -0300

    'update'

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Template Name: API 2
+Template Post Type: post, page, event
+*/
+get_header();
+$urlEmbed = 'https://api.giphy.com/v1/gifs/trending?api_key=pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa';
+
+    $responseEmbed = wp_remote_get( $urlEmbed, array(
+        'method'      => 'GET'
+        )
+    );
+     
+    if ( is_wp_error( $responseEmbed ) ) {
+        return false;
+    } else {
+        $rEmbed = wp_remote_retrieve_body( $responseEmbed );
+        $result = json_decode($rEmbed, true);
+    }
+?>
+<?php 
+
+    foreach($result['data'] as $get_result){?>
+        <img src="<?php echo $get_result['images']['480w_still']['url'] .'<br>' ;?>" width="30px" height="30">
+        
+    <?php }?>
+
+<?php get_footer();?>
